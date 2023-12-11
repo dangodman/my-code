864ef1d6d3aa7c895618eec463d23c150da200ef
dangodman <2488399989@qq.com>
2023-12-11 21:34:57 +0800

    vue-sell

---
+++ vue/vue-sell/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <Header />
+  <router-view/>
+</template>
+
+<script>
+import Header from '@/components/header/Header.vue'
+export default {
+  components: {
+    Header
+  }
+}
+</script>
+<style lang="less">
+
+</style>
