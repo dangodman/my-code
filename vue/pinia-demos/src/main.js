bd1f8a965a32a1c36c8ebdd0aaa47a87376ae519
dangodman <2488399989@qq.com>
2023-12-22 21:34:45 +0800

    pinia-demos

---
+++ vue/pinia-demos/src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+// import router from './router/index.js'
+import {createPinia} from 'pinia'
+
+
+
+const app = createApp(App)
+app
+  // .use(router) // SPA
+  .use(createPinia())  // 统一管理数据 会计
+  .mount('#app')  // UI组件 
