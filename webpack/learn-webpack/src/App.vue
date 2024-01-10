561be1bc5dd7eafd6a6fc190dd27780aff3f489f
dangodman <2488399989@qq.com>
2024-01-10 18:43:52 +0800

    learn-webpack

---
+++ webpack/learn-webpack/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="wrap">
+
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="css" scoped>
+.wrap{
+  width: 300px;
+  height: 300px;
+  background: #000;
+  color:#fff;
+  font-size: 24px;
+}
+</style>
